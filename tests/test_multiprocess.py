4453dcde9b62fff1722f5f31b8c8ed5eed2ca6dc
yannikkellerde <ykeller@mpib-berlin.mpg.de>
2024-10-19 06:35:44 +0200

    the bright side of life

--- tests/test_multiprocess.py
+++ tests/test_multiprocess.py
@@ -7,17 +7,21 @@ from ykutil.multiprocess import (
 )
 
 
-def tensor_fill(tensor, fill_value, extra_add=0):
-    for t, f in zip(tensor, fill_value):
-        t[:] = f + extra_add
+def tensor_fill(tensor, fill_value, floatadd, extra_add=0):
+    for t, f, fl in zip(tensor, fill_value, floatadd):
+        t[:] = f + fl + extra_add
     return tensor
 
 
 def test_fill_value():
     tensors = [torch.zeros(n) for n in range(4, 8)]
     extra_kwargs = {"extra_add": 1}
-    list_kws = {"fill_value": [1, 2, 3, 4], "tensor": tensors}
-    res = run_in_parallel(tensor_fill, list_kws, 4, extra_kwargs)
+    list_kws = {
+        "fill_value": [1, 2, 3, 4],
+        "tensor": tensors,
+        "floatadd": [0.1, 0.2, 0.3, 0.4],
+    }
+    res = run_in_parallel(tensor_fill, list_kws, 3, extra_kwargs)
     # for i in range(4):
     #     tensor_fill(tensors[i], list_kws["fill_value"][i], 1)
     print(tensors)
