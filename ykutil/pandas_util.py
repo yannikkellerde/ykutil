51befde06116f88ae589039452e8915a49745355
yannikkellerde <yannik@kelnet.de>
2025-05-05 15:04:39 +0200

    adding a panda

--- ykutil/pandas_util.py
+++ ykutil/pandas_util.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 
-def expand_json_cols(df: pd.DataFrame, cols: list[str]) -> pd.DataFrame:
+def expand_json_cols(df: pd.DataFrame, cols: list[str] | str) -> pd.DataFrame:
+    if isinstance(cols, str):
+        cols = [cols]
     for col in cols:
         df = pd.concat([df, pd.json_normalize(df[col])], axis=1)
         df = df.drop(columns=[col])
